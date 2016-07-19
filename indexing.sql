bb7cc45418e624a73900e484109256de1c9e9ba6
Mattdre <olsen_matthew@yahoo.com>
2016-07-18 23:50:28 -1000

    Finished first statement

--- indexing.sql
+++ indexing.sql
@@ -2,5 +2,30 @@ DROP DATABASE IF EXISTS indexed_cars;
 DROP USER IF EXISTS indexed_cars_user;
 
 CREATE USER indexed_cars_user;
-CREATE DATABASE indexed_cars
-      WITH OWNER = indexed_cars_user;
+CREATE DATABASE indexed_cars OWNER indexed_cars_user;
+
+\c indexed_cars;
+
+\i scripts/car_models.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+\i scripts/car_model_data.sql
+-----for loop for sql?
+
+SELECT COUNT (*)
+FROM car_models;
+
+SELECT DISTINCT make_title --DISTINCT eliminates duplicate rows********
+FROM car_models
+WHERE make_code = 'LAM';
+
+
+
+\c MatthewOlsen;
