c07b7a929fa7edd3dcf855338ad23b00a00f6504
Mattdre <olsen_matthew@yahoo.com>
2016-07-19 00:16:11 -1000

    Finished third statement

--- indexing.sql
+++ indexing.sql
@@ -21,10 +21,31 @@ CREATE DATABASE indexed_cars OWNER indexed_cars_user;
 
 SELECT COUNT (*)
 FROM car_models;
+\timing
 
+-- Statement 1 --
 SELECT DISTINCT make_title --DISTINCT eliminates duplicate rows********
 FROM car_models
 WHERE make_code = 'LAM';
+--//53.224 ms//--
+
+-- Statement 2 --
+SELECT DISTINCT model_title
+FROM car_models
+WHERE make_code = 'NISSAN'
+AND model_code = 'GT-R';
+--//31.747 ms//--
+
+-- Statement 3 --
+pset;
+SELECT make_code, model_code, model_title, year
+FROM car_models
+WHERE make_code = 'LAM';
+--//30.097 ms//--
+
+-- Statement 4 --
+
+
 
 
 
